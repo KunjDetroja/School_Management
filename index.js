604c3a3cb6b6208610560c03c719fc95d4b0513e
Kunj Detroja <kunjdetroja52@gmail.com>
2024-12-05 00:31:34 +0530

    Configure database connection to use port from environment variable

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const schoolRoutes = require('./routes/schoolRoutes');
-const setupDatabase = require('./setupDatabase');
 
 dotenv.config();
 
@@ -12,13 +11,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', schoolRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.DB_PORT || 3000;
 
-// Ensure database setup before starting the server
-setupDatabase().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}).catch((error) => {
-  console.error("Failed to setup database:", error.message);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
