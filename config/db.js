f59296c8f4ce64cd2bd9204b09c845b352e8c101
Kunj Detroja <kunjdetroja52@gmail.com>
2024-12-05 00:45:47 +0530

    Remove unused database setup script and Postman collection

--- config/db.js
+++ config/db.js
@@ -16,27 +16,3 @@ const pool = mysql.createPool({
 
 module.exports = pool;
 
-
-// const mysql = require('mysql2/promise');
-// const dotenv = require('dotenv');
-// const fs = require('fs');
-
-// dotenv.config();
-
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST, // Remote database host
-//   user: process.env.DB_USER, // Remote database username
-//   password: process.env.DB_PASSWORD, // Remote database password
-//   database: process.env.DB_NAME, // Remote database name
-//   port: process.env.DB_PORT || 3306, // MySQL port
-//   ssl: process.env.DB_SSL === 'true' ? {
-//     rejectUnauthorized: true,
-//     ca: fs.readFileSync('./ca.pem'), // Optional, if CA certificate is required
-//   } : false,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// });
-
-// module.exports = pool;
-
